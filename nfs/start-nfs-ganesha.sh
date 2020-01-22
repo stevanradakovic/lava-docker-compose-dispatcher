4c9f0129779dd2adaf08a2fc0922540cd0d89c98
stevanradakovic <stevan.radakovic@linaro.org>
2020-01-22 09:11:18 +0100

    nfsrootfs updates for beaglebone black test jobs

--- nfs/start-nfs-ganesha.sh
+++ nfs/start-nfs-ganesha.sh
@@ -1,33 +1,20 @@
 #!/bin/sh
 
-init_rpc() {
+init_services() {
     echo "* Starting rpcbind"
-    if [ ! -x /run/rpcbind ] ; then
-        install -m755 -g 32 -o 32 -d /run/rpcbind
-    fi
-    rpcbind || return 0
-    rpc.statd -L || return 0
-    rpc.idmapd || return 0
-    sleep 1
-}
-
-init_dbus() {
+    mkdir -p /run/sendsigs.omit.d/
+    service rpcbind start
+    echo "* Starting nfs-common"
+    service nfs-common start
     echo "* Starting dbus"
-    if [ ! -x /var/run/dbus ] ; then
-        install -m755 -g 81 -o 81 -d /var/run/dbus
-    fi
+    mkdir -p /var/run/dbus
+    chmod 755 /var/run/dbus
     rm -f /var/run/dbus/*
     rm -f /var/run/messagebus.pid
     dbus-uuidgen --ensure
     dbus-daemon --system --fork
     sleep 1
 }
-#rpcbind
-#rpc.statd -L
-#rpc.idmapd
-#dbus-uuidgen --ensure
-#dbus-daemon --system --fork
-init_rpc
-init_dbus
 
+init_services
 exec /usr/bin/ganesha.nfsd -F -L /dev/stdout -f /etc/ganesha/ganesha.conf
